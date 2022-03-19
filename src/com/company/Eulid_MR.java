b8e3f51f6d1201e24addedd70fd2f003df16cd08
Hồ Minh Cảnh <minhcanhbinhdai@gmail.com>
2022-03-19 14:21:27 +0700

    Initial commit

---
+++ src/com/company/Eulid_MR.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Eulid_MR {
+    So euclid(int a,int b){
+        Arraylyst <So> st = new Arraylyst;
+        return null;
+    }
+}
---
+++ src/com/company/So.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class So {
+    private int a,b;
+    public So(){
+
+    }
+    public So(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+    int getA(){
+        return a;
+    }
+    int getB(){
+        return b;
+    }
+    void setA(int a){
+        this.a=a;
+    }
+    void setB(int b){
+        this.b=b;
+    }
+}
